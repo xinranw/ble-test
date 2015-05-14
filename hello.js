4853bfef95ebffd952cedafb8e0f1ea6a503fb60
Xinran Wang <xw1992@gmail.com>
2015-05-14 17:50:45 +0000

    Multiple robots.

--- hello.js
+++ hello.js
@@ -1,24 +1,25 @@
 var Cylon = require('cylon');
 
-var print = Cylon.robot({
-  work: function() {
-    every((1).second(), function() {
-      console.log("Hello, human!");
-    });
-  }
-});
-
 function writeToScreen(screen, message) {
   screen.setCursor(0,0);
   screen.write(message);
 }
 
-var lcd = Cylon
-  .robot({ name: 'LCD'})
-  .connection('edison', { adaptor: 'intel-iot' })
-  .device('screen', { driver: 'upm-jhd1313m1', connection: 'edison' })
-  .on('ready', function(my) {
-    writeToScreen(my.screen, "Ready!");
-    my.screen.clear();
+Cylon
+.robot({ name: 'LCD'})
+.connection('edison', { adaptor: 'intel-iot' })
+.device('screen', { driver: 'upm-jhd1313m1', connection: 'edison' })
+.on('ready', function(my) {
+  writeToScreen(my.screen, "Ready!");
+});
+
+Cylon
+.robot({name:'Hello'})
+.on('ready', function(){
+  every((1).second(), function(){
+    console.dir("hello!!");
   });
-lcd.start();
+});
+
+Cylon.start();
+
