1eabd1329b761f2a4dd026b7d70b4fcbbe8a816d
Xinran Wang <xw1992@gmail.com>
2015-05-14 12:14:16 -0400

    LCD works now.

--- hello.js
+++ hello.js
@@ -1,9 +1,24 @@
 var Cylon = require('cylon');
 
-Cylon.robot({
+var print = Cylon.robot({
   work: function() {
     every((1).second(), function() {
       console.log("Hello, human!");
     });
   }
-}).start();
+});
+
+function writeToScreen(screen, message) {
+  screen.setCursor(0,0);
+  screen.write(message);
+}
+
+var lcd = Cylon
+  .robot({ name: 'LCD'})
+  .connection('edison', { adaptor: 'intel-iot' })
+  .device('screen', { driver: 'upm-jhd1313m1', connection: 'edison' })
+  .on('ready', function(my) {
+    writeToScreen(my.screen, "Ready!");
+    my.screen.clear();
+  });
+lcd.start();
